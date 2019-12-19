37981f4a94bb094fb0729bc37d85b16347184e2c
Ali Barakat <a.barakat@vanlanschotkempen.com>
2019-12-19 16:32:02 +0100

    Updates

---
+++ precache-manifest.5277df78072a5d5d1db4e2b966be8c26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42741c063117ddb319f8628cc6257c7e",
+    "url": "/Ali-Barakat/reactUltra/index.html"
+  },
+  {
+    "revision": "422e6a61d2a619d94d78",
+    "url": "/Ali-Barakat/reactUltra/static/css/main.fdb24fcf.chunk.css"
+  },
+  {
+    "revision": "63402ca66cb2282ef9b2",
+    "url": "/Ali-Barakat/reactUltra/static/js/2.aaf88453.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/Ali-Barakat/reactUltra/static/js/2.aaf88453.chunk.js.LICENSE"
+  },
+  {
+    "revision": "422e6a61d2a619d94d78",
+    "url": "/Ali-Barakat/reactUltra/static/js/main.948f3746.chunk.js"
+  },
+  {
+    "revision": "8cc41ac237a7ac8ff468",
+    "url": "/Ali-Barakat/reactUltra/static/js/runtime-main.f761ca4f.js"
+  }
+]);
